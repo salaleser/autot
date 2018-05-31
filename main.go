1e5f93338eef840643a7dceda637ad1f7c9118f4
Алексей Салиенко <30593575+salaleser@users.noreply.github.com>
2018-05-31 15:08:10 +0300

    причесал

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"os/exec"
 	"strings"
@@ -12,12 +13,54 @@ import (
 	"github.com/sbstjn/hanu"
 )
 
+const (
+	statusRunning = "4  RUNNING"
+	process       = "sc"
+	server        = "\\\\Kmisdevserv"
+	service       = "IBM Domino Server (DDOMINODATA)"
+)
+
+var (
+	running     bool
+	lastStarted int64
+	lastStopped int64
+)
+
 type config struct {
 	Token   string `json:"token"`
 	Version string `json:"version"`
 }
 
+func loopQuery() {
+	for {
+		out := runCommand("query")
+		running = strings.Contains(out, statusRunning)
+		if running {
+			if lastStarted < lastStopped {
+				lastStarted = time.Now().UnixNano()
+				fmt.Println("Служба запущена.")
+			}
+		} else {
+			if lastStarted > lastStopped {
+				lastStopped = time.Now().UnixNano()
+				fmt.Println("Служба остановлена!")
+			}
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+func runCommand(s string) string {
+	out, err := exec.Command(process, server, s, service).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(out)
+}
+
 func main() {
+
 	f, err := os.Open("config.json")
 	if err != nil {
 		log.Fatal("Конфигурационный файл не найден!")
@@ -40,43 +83,58 @@ Reconnect:
 	slack, err := hanu.New(response.Token)
 	if err != nil {
 		log.Println(err)
-		fmt.Print("Reconnecting... ")
+		fmt.Print("Reconnecting in...")
 		for i := 30; i > 0; i-- {
 			time.Sleep(time.Second)
-			fmt.Print(i)
-			fmt.Print(" ")
+			fmt.Println(i, " ")
 		}
-		fmt.Println()
-		// log.Fatal(err)
 		goto Reconnect
 	}
 
 	fmt.Println("Connected!")
 
-	slack.Command("shout <word>", func(conv hanu.ConversationInterface) {
-		str, _ := conv.String("word")
-		conv.Reply(strings.ToUpper(str))
-	})
+	go loopQuery()
 
-	slack.Command("<word>", func(conv hanu.ConversationInterface) {
-		str, _ := conv.String("word")
-		fmt.Println(str)
-	})
+	// slack.Command("shout <word>", func(conv hanu.ConversationInterface) {
+	// 	str, _ := conv.String("word")
+	// 	conv.Reply(strings.ToUpper(str))
+	// })
 
-	slack.Command("q", func(conv hanu.ConversationInterface) {
-		out, err := exec.Command("sc").Output()
-		if err != nil {
-			log.Println(err)
+	slack.Command("query", func(conv hanu.ConversationInterface) {
+		var msg string
+		if running {
+			rndSource := rand.NewSource(time.Now().UnixNano())
+			rndRand := rand.New(rndSource)
+			rndInt := rndRand.Intn(100)
+			if rndInt > 90 {
+				msg = "лотусист спит, служба идет..."
+			} else {
+				msg = "служба бежит..."
+			}
+		} else {
+			msg = "служба лежит!"
 		}
-		conv.Reply("%s", out)
+		conv.Reply("%s", msg)
+	})
+
+	slack.Command("stop", func(conv hanu.ConversationInterface) {
+		runCommand("stop")
+		conv.Reply("%s", "останавливаю...")
+	})
+
+	slack.Command("start", func(conv hanu.ConversationInterface) {
+		runCommand("start")
+		conv.Reply("%s", "запускаю...")
 	})
 
 	slack.Command("version", func(conv hanu.ConversationInterface) {
-		conv.Reply("Thanks for asking! I'm running `%s`", response.Version)
+		conv.Reply("`%s`", response.Version)
 	})
 
-	slack.Command("help", func(conv hanu.ConversationInterface) {
-		conv.Reply("`q` — тест", response.Version)
+	slack.Command("?", func(conv hanu.ConversationInterface) {
+		conv.Reply("`query` — проверка состояния службы;\n" +
+			"`start` — запустить службу;\n" +
+			"`stop` — остановить службу.")
 	})
 
 	slack.Listen()
