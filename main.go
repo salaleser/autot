4ab823692301089e15fa74c6fccf068aab69ad15
Алексей Салиенко <30593575+salaleser@users.noreply.github.com>
2018-07-04 20:37:29 +0300

    Добавил приложение в трее

--- main.go
+++ main.go
@@ -2,6 +2,7 @@
 // TODO Тут будет описание
 //
 
+// Ошибки:
 // [SC] ControlService: ошибка: 1061:
 // Служба в настоящее время не может принимать команды.
 
@@ -11,58 +12,77 @@
 // [SC] StartService: ошибка: 1056:
 // Одна копия службы уже запущена.
 
-// FIXME На данный момент бот не умеет понимать префикс, сейчас префикс захардкожен в сами команды
+// [SC] OpenSCManager: ошибка: 1722:
+// Сервер RPC недоступен.
+
+// TODO На данный момент бот не умеет понимать префикс, сейчас префикс захардкожен в сами команды
 // ...
 
 package main
 
 import (
-	"encoding/json"
+	"archive/zip"
 	"fmt"
+	"io"
 	"log"
 	"math/rand"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/getlantern/systray"
 	"github.com/sbstjn/hanu"
+	"salaleser.ru/autot/icon"
 )
 
 const (
-	countdown = 0 // количество секунд до остановки службы
+	ver        = "0.5"
+	countdown  = 6 // количество секунд до остановки службы
+	sourcePath = "\\\\Kmisdevserv\\dominodata\\"
+	destPath   = "\\\\KSERVER\\!Common\\КМИС ОП"
 )
 
 var (
-	status           int8 // 1 -- остановлена, 2 -- запускается, 3 -- останавливается, 4 -- запущена
-	lastStartPending int64
-	lastStopPending  int64
-	lastStopped      int64
-	lastStarted      int64
-
+	status   int8 // 1 -- остановлена, 2 -- запускается, 3 -- останавливается, 4 -- запущена
 	statuses = []string{
-		"0  ВАНИЛЬНЫЙ_СТАТУС",
+		"0  ВАНИЛЬНЫЙ_СТАТУС", // этот элемент никогда не используется
 		"1  STOPPED",
 		"2  START_PENDING",
 		"3  STOP_PENDING",
 		"4  RUNNING",
 	}
+	tooltips = []string{
+		"Ванильный статус", // этот элемент никогда не используется
+		"Служба остановлена.",
+		"Служба запускается…",
+		"Служба останавливается!",
+		"Служба запущена.",
+	}
+
+	lastStartPending int64
+	lastStopPending  int64
+	lastStopped      int64
+	lastStarted      int64
+
+	about = "Autot (lite) " + ver
+
+	cooldown = 500
+
+	files []string
 
 	hook hanu.ConversationInterface // FIXME это костыль, смотри комментарий к команде @hook
+	item *systray.MenuItem
 )
 
-type config struct {
-	Token   string `json:"token"`
-	Version string `json:"version"`
-}
-
 func execute(s string) string {
-	out, err := exec.Command("sc", "\\\\Kmisdevserv", s, "IBM Domino Server (DDOMINODATA)").Output()
+	o, err := exec.Command("sc", "\\\\Kmisdevserv", s, "IBM Domino Server (DDOMINODATA)").Output()
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
-	return string(out)
+	return string(o)
 }
 
 // Вечный цикл проверки статуса.
@@ -77,65 +97,108 @@ func loop() {
 			status = 4
 			if lastStopped >= lastStarted {
 				lastStarted = time.Now().Unix()
-				log.Println("Служба запущена.")
+				log.Println(tooltips[status])
+				item.SetTitle("Остановить")
+				// TODO звуковой сигнал
+				systray.SetIcon(icons.Green)
+				systray.SetTooltip(tooltips[status])
 				if hook != nil {
-					hook.Reply("%s", "*СЛУЖБА ЗАПУЩЕНА*")
+					hook.Reply("*СЛУЖБА ЗАПУЩЕНА*")
 				}
 			}
 		} else if strings.Contains(out, statuses[1]) {
 			status = 1
 			if lastStartPending >= lastStopped {
 				lastStopped = time.Now().Unix()
-				log.Println("Служба остановлена.")
+				log.Println(tooltips[status])
+				item.SetTitle("Запустить")
+				// TODO звуковой сигнал
+				systray.SetIcon(icons.Red)
+				systray.SetTooltip(tooltips[status])
 				if hook != nil {
-					hook.Reply("%s", "*СЛУЖБА ОСТАНОВЛЕНА*")
+					hook.Reply("*СЛУЖБА ОСТАНОВЛЕНА*")
 				}
 			}
 		} else if strings.Contains(out, statuses[2]) {
 			status = 2
 			if lastStopPending >= lastStartPending {
 				lastStartPending = time.Now().Unix()
-				log.Println("Служба запускается.")
+				log.Println(tooltips[status])
+				item.SetTitle(tooltips[status])
+				// TODO звуковой сигнал
+				systray.SetIcon(icons.Yellow)
+				systray.SetTooltip(tooltips[status])
 				if hook != nil {
-					hook.Reply("%s", "*СЛУЖБА ЗАПУСКАЕТСЯ*")
+					hook.Reply("*СЛУЖБА ЗАПУСКАЕТСЯ*")
 				}
 			}
 		} else if strings.Contains(out, statuses[3]) {
 			status = 3
 			if lastStarted >= lastStopPending {
 				lastStopPending = time.Now().Unix()
-				log.Println("Служба останавливается!")
+				log.Println(tooltips[status])
+				item.SetTitle(tooltips[status])
+				// TODO звуковой сигнал
+				systray.SetIcon(icons.Red)
+				systray.SetTooltip(tooltips[status])
 				if hook != nil {
-					hook.Reply("%s", "*СЛУЖБА ОСТАНАВЛИВАЕТСЯ*")
+					hook.Reply("*СЛУЖБА ОСТАНАВЛИВАЕТСЯ*")
 				}
 			}
 		}
-		time.Sleep(500 * time.Millisecond)
+		time.Sleep(time.Duration(cooldown) * time.Millisecond)
 	}
 }
 
+func onReady() {
+	systray.SetIcon(icons.Yellow)
+	systray.SetTitle("Autot")
+	systray.SetTooltip("Автотправитель")
+	go func() {
+		systray.AddMenuItem(about, "Автотправитель")
+		systray.AddSeparator()
+		item = systray.AddMenuItem("…", "")
+		mQuit := systray.AddMenuItem("Выход", "")
+		for {
+			select {
+			case <-item.ClickedCh:
+				if status == 4 {
+					go execute("stop")
+				} else if status == 1 {
+					go execute("start")
+				}
+			case <-mQuit.ClickedCh:
+				systray.Quit()
+				os.Exit(0)
+			}
+		}
+	}()
+}
+
 func main() {
-	f, err := os.Open("config.json")
-	if err != nil {
-		log.Fatal("Конфигурационный файл не найден!")
+	go loop()
+
+	onExit := func() {
+		// Пример лога. Может быть добавлю что-то подобное
+		// now := time.Now()
+		// ioutil.WriteFile(fmt.Sprintf(`on_exit_%d.txt`, now.UnixNano()), []byte(now.String()), 0644)
 	}
 
-	body := make([]byte, 86) //FIXME hardcode!
-
-	n1, err := f.Read(body)
-	if err != nil {
-		fmt.Printf("%d bytes: %s\n", n1, string(body))
+	if len(os.Args) > 1 {
+		arg := os.Args[1]
+		if arg[:5] != "xoxb-" { // TODO добавить проверку паттерном регекспа
+			log.Fatal("Неправильный токен! Попробуйте указать токен slack-бота заново.")
+		}
+		go client(arg)
 	}
+	systray.Run(onReady, onExit)
+}
 
-	var response config
-	err = json.Unmarshal(body, &response)
-	if err != nil {
-		fmt.Printf("Failed to unmarshal JSON: %s", body)
-	}
-
+func client(token string) {
+	about = "Autot " + ver
 Reconnect:
 	log.Println("Connecting...")
-	slack, err := hanu.New(response.Token)
+	slack, err := hanu.New(token)
 	if err != nil {
 		log.Println(err)
 		fmt.Print("Reconnecting in... ")
@@ -144,14 +207,11 @@ Reconnect:
 			fmt.Print(i, " ")
 		}
 		fmt.Println("0")
-		goto Reconnect // goto нормальная тема
+		goto Reconnect
 	}
-
 	log.Println("Connected!")
 
 	opStatus := make(chan bool) // канал для отмены остановки
-
-	go loop() // Андрею не понравится, но по-моему так читается легче
 
 	var cmd hanu.Command
 	cmd = hanu.NewCommand("!query", "проверяет состояние службы", // FIXME hardcode
@@ -176,11 +236,12 @@ Reconnect:
 					text = "Служба работает."
 				}
 			}
-			conv.Reply("%s", text)
+			conv.Reply(text)
 		})
 	slack.Register(cmd)
 
-	cmd = hanu.NewCommand("!stop", "останавливает службу", // FIXME hardcode
+	cmd = hanu.NewCommand("!stop", // FIXME hardcode
+		"останавливает службу через указанное количество секунд",
 		func(conv hanu.ConversationInterface) {
 			var text string
 			var cancelled bool
@@ -193,12 +254,12 @@ Reconnect:
 				text = "Терпение, служба уже останавливается!"
 			case 4:
 				// TODO добавить отправку предупреждения в скайп
+				conv.Reply("*ВНИМАНИЕ!*\nСлужба будет остановлена через " +
+					strconv.Itoa(countdown) + " секунд!")
 				for i := countdown; i > 0; i-- {
-					conv.Reply("%s", "Служба будет остановлена через "+
-						strconv.Itoa(i)+" сек. (`!cancel` — отмена)")
 					select {
 					case <-opStatus:
-						conv.Reply("%s", "Остановка службы отменена.")
+						conv.Reply("Остановка службы отменена.")
 						// TODO добавить отправку предупреждения в скайп
 						cancelled = false // TODO вроде избыточная переменная, лучше придумать надо
 						break
@@ -211,7 +272,7 @@ Reconnect:
 					text = "Останавливаю…"
 				}
 			}
-			conv.Reply("%s", text)
+			conv.Reply(text)
 		})
 	slack.Register(cmd)
 
@@ -229,7 +290,7 @@ Reconnect:
 			case 4:
 				text = "Служба уже запущена!"
 			}
-			conv.Reply("%s", text)
+			conv.Reply(text)
 		})
 	slack.Register(cmd)
 
@@ -240,14 +301,14 @@ Reconnect:
 				opStatus <- true
 				fmt.Println("opStatus <- true") //debug
 			} else {
-				conv.Reply("%s", "Службу не планировалось останавливать.")
+				conv.Reply("Службу не планировалось останавливать.")
 			}
 		})
 	slack.Register(cmd)
 
 	cmd = hanu.NewCommand("!version", "версия", // FIXME hardcode
 		func(conv hanu.ConversationInterface) {
-			conv.Reply("`%s`", response.Version)
+			conv.Reply(ver)
 		})
 	slack.Register(cmd)
 
@@ -257,36 +318,168 @@ Reconnect:
 		func(conv hanu.ConversationInterface) {
 			if hook == nil {
 				hook = conv
-				hook.Reply("%s", "Канал зохвачен!"+
-					" (Оповещения об изменении состояния Службы будут приходить сюда)")
+				hook.Reply("Оповещения об изменении состояния Службы будут приходить в этот канал.")
 			}
 		})
 	slack.Register(cmd)
 
-	cmd = hanu.NewCommand("!db <файлы,через,запятую>", "обновляет список отправляемых баз данных",
+	cmd = hanu.NewCommand("!add <файлы,через,запятую>", "обновляет список отправляемых баз данных",
 		func(conv hanu.ConversationInterface) {
-			conv.Reply("%s", "Список баз данных обновляется…")
 			s, _ := conv.String("файлы,через,запятую")
-			files := strings.Split(s, ",")
-			// TODO обработать файлы
-			conv.Reply("%s", "Список баз данных обновлен."+
-				" Дайте команду `!send` для отправки их в ОП.")
-			text := "Список баз данных, готовых к отправке:```"
+			files = strings.Split(s, ",")
+			conv.Reply("Список файлов обновлен. Дайте команду `!push` для отправки их в ОП.")
+			text := ""
 			for i := 1; i <= len(files); i++ {
 				n := strconv.FormatInt(int64(i), 10)
 				text += n + ". " + files[i-1] + "\n"
 			}
-			conv.Reply("%s", text+"```")
+			conv.Reply("```" + text + "```")
 		})
 	slack.Register(cmd)
 
-	cmd = hanu.NewCommand("!send ", "копирует указанные в списке !db базы данных в ОП",
+	cmd = hanu.NewCommand("!push", "копирует указанные в списке !db базы данных в ОП",
 		func(conv hanu.ConversationInterface) {
-			conv.Reply("%s", "Начинаю копирование…")
-			// TODO Добавить копирование файлов
-			conv.Reply("%s", "Копирование файлов завершено.")
+			if len(files) == 0 {
+				conv.Reply("Список файлов пустой! Воспользуйтесь сначала командой `!add`.")
+				return
+			}
+			conv.Reply("Начинаю упаковку…")
+			y, m, d := time.Now().Date()
+			date := strconv.Itoa(y) + "-" + m.String() + "-" + strconv.Itoa(d)
+			fileName := "Templates_" + date + "_KMIS.zip"
+			if err := zipFiles(fileName, files); err != nil {
+				log.Fatal(err)
+			}
+			conv.Reply("Упаковка файлов завершена.")
+
+			conv.Reply("Начинаю копирование…")
+			_, err := exec.Command("xcopy", fileName, destPath, "/Y").Output()
+			if err != nil {
+				log.Fatal(err)
+			}
+			conv.Reply("Копирование архива `" + fileName + "` в КМИС ОП завершено.")
+		})
+	slack.Register(cmd)
+
+	cmd = hanu.NewCommand("!pull", "ставит подписанные шаблоны (в разработке)",
+		func(conv hanu.ConversationInterface) {
+			conv.Reply("Начинаю распаковку…")
+			signedTemplates, err := Unzip(destPath+
+				"\\Подписанные\\"+"Templates_388_2018-06-21_KMIS.7z", "temp")
+			if err != nil {
+				log.Fatal(err)
+			}
+			conv.Reply("Распаковка файлов завершена.")
+
+			conv.Reply("Начинаю копирование…")
+			for _, template := range signedTemplates {
+				_, err := exec.Command("xcopy", "temp\\"+template, "sourcePath", "/Y").Output()
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+			conv.Reply("Установка подписанных шаблонов завершена. //тест")
 		})
 	slack.Register(cmd)
 
 	slack.Listen()
+}
+
+func zipFiles(filename string, files []string) error {
+	newfile, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer newfile.Close()
+
+	zipWriter := zip.NewWriter(newfile)
+	defer zipWriter.Close()
+
+	for _, file := range files {
+
+		zipfile, err := os.Open(sourcePath + file)
+		if err != nil {
+			return err
+		}
+		defer zipfile.Close()
+
+		info, err := zipfile.Stat()
+		if err != nil {
+			return err
+		}
+
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		// Change to deflate to gain better compression
+		// see http://golang.org/pkg/archive/zip/#pkg-constants
+		header.Method = zip.Deflate
+
+		writer, err := zipWriter.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(writer, zipfile)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Unzip will decompress a zip archive, moving all files and folders
+// within the zip file (parameter 1) to an output directory (parameter 2).
+func Unzip(src string, dest string) ([]string, error) {
+
+	var filenames []string
+
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return filenames, err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+
+		rc, err := f.Open()
+		if err != nil {
+			return filenames, err
+		}
+		defer rc.Close()
+
+		// Store filename/path for returning and using later on
+		fpath := filepath.Join(dest, f.Name)
+		filenames = append(filenames, fpath)
+
+		if f.FileInfo().IsDir() {
+
+			// Make Folder
+			os.MkdirAll(fpath, os.ModePerm)
+
+		} else {
+
+			// Make File
+			if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+				return filenames, err
+			}
+
+			outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return filenames, err
+			}
+
+			_, err = io.Copy(outFile, rc)
+
+			// Close the file without defer to close before next iteration of loop
+			outFile.Close()
+
+			if err != nil {
+				return filenames, err
+			}
+
+		}
+	}
+	return filenames, nil
 }
